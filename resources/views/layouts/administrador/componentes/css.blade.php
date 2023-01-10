068082632f9f063b166e12756260eda7f0668d25
Mersmith <mersmith14@gmail.com>
2023-01-10 17:40:09 -0500

    Crud Categoria

---
+++ resources/views/layouts/administrador/componentes/css.blade.php
@@ -0,0 +1,2 @@
+<!-- Fontawesome -->
+<link rel="stylesheet" href="{{ asset('vendor/fontawesome-free-6.1.2-web/css/all.min.css') }}">
--- routes/administrador.php
+++ routes/administrador.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Livewire\Administrador\Categoria\CategoriaLivewire;
 use Illuminate\Support\Facades\Route;
 
 Route::get('prueba-administrador', function () {
     return "Amdministrador";
 });
+
+Route::get('categorias', CategoriaLivewire::class)->name('categoria.index');
