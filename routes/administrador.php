1e6782b9ae92b7e58e1faba42e234471d286df71
Mersmith <mersmith14@gmail.com>
2023-01-12 12:37:11 -0500

    Crud cliente

--- routes/administrador.php
+++ routes/administrador.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\Administrador\Ckeditor5Controller;
 use App\Http\Controllers\Administrador\ProductoController;
 use App\Http\Livewire\Administrador\Categoria\CategoriaLivewire;
+use App\Http\Livewire\Administrador\Cliente\ClienteCrearLivewire;
+use App\Http\Livewire\Administrador\Cliente\ClienteEditarLivewire;
+use App\Http\Livewire\Administrador\Cliente\ClienteTodoLivewire;
 use App\Http\Livewire\Administrador\Producto\ProductoCrearLivewire;
 use App\Http\Livewire\Administrador\Producto\ProductoEditarLivewire;
 use App\Http\Livewire\Administrador\Producto\ProductoTodoLivewire;
@@ -22,3 +25,8 @@ Route::get('producto/crear', ProductoCrearLivewire::class)->name('producto.crear
 Route::get('producto/{producto}/editar', ProductoEditarLivewire::class)->name('producto.editar');
 Route::post('ckeditor-upload', [Ckeditor5Controller::class, 'upload'])->name('ckeditor.upload');
 Route::post('producto/{producto}/dropzone', [ProductoController::class, 'dropzone'])->name('producto.dropzone');
+
+Route::get('clientes', ClienteTodoLivewire::class)->name('cliente.index');
+Route::get('cliente/crear', ClienteCrearLivewire::class)->name('cliente.crear');
+Route::get('cliente/{cliente}/editar', ClienteEditarLivewire::class)->name('cliente.editar');
+
